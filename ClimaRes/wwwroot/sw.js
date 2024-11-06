2deda8c42f50d251ab20f068a6cdb7a5db7f8e11
Madelisa-luna <pilarelisamalunaaguilar@gmail.com>
2024-11-06 09:04:05 -0600

    Intervencio para que no funcione la hoja de estilos

--- ClimaRes/wwwroot/sw.js
+++ ClimaRes/wwwroot/sw.js
@@ -12,8 +12,17 @@ self.addEventListener('activate', e => {
 //imagenes, .css, .js, .html o llamadas asincrona
 self.addEventListener('fetch', e => {
     //1. Mostrar las llamadas al servidor
-    console.log(e.request.url)
+    //console.log(e.request.url)
 
     //2. Requiere que las llamadas al servidor las haga el service worker
-    e.respondWith(fetch(e.request.url))
+    //e.respondWith(fetch(e.request.url))
+
+    //3. Si la url es la siguiente}
+    //if (e.request.url == "http://localhost:5124/css/Estilo.css") {
+        //No traigas el archivo
+     
+        //e.respondWith(null)
+   // }
+    //Si no trae normal el archivo
+    //else e.respondWith(fetch(e.request.url))
 });
